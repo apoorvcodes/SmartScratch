375ee4eb9986ce5376c84e836c6e3ac7309e10ef
megatank58 <tanumegatank@gmail.com>
2022-06-03 10:03:53 +0000

    chore: add initial deps

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from "nuxt";
+
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  plugins: [require('daisyui')],
+};
